728fe21f21b10390a19fd9c58ddd5b46cb4aa5a1
Andoru <bravocastro.andresmartin@gmail.com>
2023-10-24 15:27:38 +0200

    Version 19.10.23 ImprimirTriangulo_terminado

--- EjerciciosEnClase/src/EjerciciosPseint/ImprimirTriangulo.java
+++ EjerciciosEnClase/src/EjerciciosPseint/ImprimirTriangulo.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class ImprimirTriangulo {
 
+    //atributos
     static Scanner lector = new Scanner(System.in);
 
     //-- METODOS PRINCIPALES --//
@@ -11,33 +12,41 @@ public class ImprimirTriangulo {
         int tamTriangulo;
         System.out.println("Ingresa el tamaño de la base del triangulo");
 
+        // lectura de  datos
         tamTriangulo = pedirNumero();
 
-        for (int contAste = 1; contAste < tamTriangulo; contAste++) {
+        // imprime un salto de linea después de imprimir el asteriscto
+        for (int contBla = 1; contBla <= tamTriangulo; contBla++) {
 
-            for (int contBla = 1; contBla < (contAste - 1) ; contBla++) {
+            // imprime asteriscos desde 0 hasta (n - 1) en cada fila
+            for (int conAste = 0; conAste <= (contBla - 1) ; conAste++) {
                 System.out.print("*");
             }
-            for (int contBla = 1; contBla < (tamTriangulo - 1) ; contBla++) {
-                System.out.print(" ");
-            }
+
             System.out.println(" ");
 
-        }
+        }// fin for
 
-
-    }
+    }// fin ejecutarPrograma
 
     //-- METODOS SECUNDARIOS --//
+
+    /**
+     * función que pide un número positivo
+     * @return nIngresado entero
+     */
     public static int pedirNumero() {
 
+        // atributos
         int nIngresado;
         nIngresado = lector.nextInt();
 
+        // compruebueba que el numero ingresado no es negativo
         while (nIngresado <= 0) {
             System.out.println("Error: no puede ser menor o igual a 0");
             nIngresado = lector.nextInt();
         }
+
         return nIngresado;
 
     }// fin pedirNumero
