03118f91c52d0b5415596381c197897b826fd130
alumnoDAW <bravocastro.andresmartin@gmail.com>
2023-10-27 10:50:54 +0200

    Version 27.10 Actividades UT02 - Entrada de datos hecho 1 al 7 pero  falta comentar todos - Enum 1 hecho y 2 empezado - Ejercicios Leer edad en clase - Leer un double con punto en vez de coma

--- EjerciciosEnClase/src/ActividadesUT02/EntradaDatos/Ejercicio5.java
+++ EjerciciosEnClase/src/ActividadesUT02/EntradaDatos/Ejercicio5.java
@@ -1,10 +1,12 @@
 package ActividadesUT02.EntradaDatos;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Ejercicio5 {
     // atributos
     static Scanner lector = new Scanner(System.in);
+
 
     // <-- Metodos pricipales -->
     public static void ejecutarPrograma(){
@@ -12,7 +14,7 @@ public class Ejercicio5 {
 
         System.out.printf ( "Introduce la temperatura en grados centigrados para mostrar su equivalente en Fahrenheit, Kelvin y Reamur%n" );
 
-        gCent = lector.nextDouble ();
+        gCent = obtNumero();
         // de celcius a kelvin
         gKelvin = gCent + 273.15;
 
@@ -22,9 +24,9 @@ public class Ejercicio5 {
         // de celcius a Reamur
         gReamur = gCent * 0.8;
 
-        System.out.printf ( "%nEl equivalente en grados Kelvin es: %.2f%n" , gKelvin );
-        System.out.printf ( "%nEl equivalente en grados Fahrenhait es: %.2f%n" , gFaren );
-        System.out.printf ( "%nEl equivalente en grados Reamur es: %.2f%n" , gReamur );
+        System.out.printf ( "El equivalente en grados Kelvin es: %.2f%n" , gKelvin );
+        System.out.printf ( "El equivalente en grados Fahrenhait es: %.2f%n" , gFaren );
+        System.out.printf ( "El equivalente en grados Reamur es: %.2f%n" , gReamur );
 
     }// fin-ejecutarPrograma
 
@@ -32,6 +34,8 @@ public class Ejercicio5 {
     public static double obtNumero (){
 
         double nIngresado;
+
+        lector.useLocale(Locale.ENGLISH);// permite leer un double con punto
 
         nIngresado = lector.nextDouble ();
 
--- EjerciciosEnClase/src/ActividadesUT02/EntradaDatos/ExeClass.java
+++ EjerciciosEnClase/src/ActividadesUT02/EntradaDatos/ExeClass.java
@@ -8,9 +8,9 @@ public class ExeClass {
         //Ejercicio2.ejecutarPrograma ();
         //Ejercicio3.ejecutarPrograma ();
         //Ejercicio4.ejecutarPrograma ();
-        //Ejercicio5.ejecutarPrograma ();
+        Ejercicio5.ejecutarPrograma ();
         //Ejercicio6.ejecutarPrograma ();
-        Ejercicio7.ejecutarPrograma ();
+        //Ejercicio7.ejecutarPrograma ();
 
 
     }
