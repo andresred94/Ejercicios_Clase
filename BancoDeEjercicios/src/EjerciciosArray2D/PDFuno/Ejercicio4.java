4c2e0a2d5cb559aba125da6f5a76f58681d73f50
Andoru <bravocastro.andresmartin@gmail.com>
2023-12-14 18:10:06 +0100

    Versión 14.12 EjerciciosArrays2D PDFuno - Terminados (1,2,3,4)

--- BancoDeEjercicios/src/EjerciciosArray2D/PDFuno/Ejercicio4.java
+++ BancoDeEjercicios/src/EjerciciosArray2D/PDFuno/Ejercicio4.java
@@ -5,49 +5,7 @@ import java.util.Arrays;
 public class Ejercicio4 {
 
     public static void main ( String[] args ) {
-        int [][] matrix3x3 = new int[3][3];
-
-        // Recorre cada fila
-        for ( int i = 0 ; i < matrix3x3.length ; i++ ) {
-            // Recorre cada columna de cada fila
-            for ( int j = 0 ; j < matrix3x3[i].length ; j++ ) {
-                // Se genera y se ingrea un número aleatorio del 1 al 9
-                int nRan = azar ();
-                matrix3x3[i][j] =  nRan;
-                /*// recorre cada fila y verifica si ya existe ese número en la fila actual
-                for (int k = 0; k < j; k++) {
-                    if (matrix3x3[i][j] == matrix3x3[i][k]) {
-                        matrix3x3[i][j] = azar ();
-                        j--; // Reintentar generar un nuevo número en la misma posición
-                        break;
-                    }
-                }// for-k*/
-
-                /*// Recorre cada posición de la matriz y verifica si la posición anterior es igual a la siguiente
-                for (int m = 0; m < i; m++) {// filas
-                    for (int n = 0; n < matrix3x3[m].length; n++) {// colum
-                        // Si son iguales
-                        if (matrix3x3[i][j] == matrix3x3[m][n]) {
-                            matrix3x3[i][j] = azar(); // Genera un nuevo número
-                            m = -1; // Reinicia la verificación desde la primera fila
-                            break;
-                        }
-
-                    }// for-n
-                }// for-m*/
-
-                // Verificar el nuevo número generado en todas las filas anteriores
-                for (int m = 0; m < i ; m++) {
-                    for (int n = 0; n < matrix3x3[m].length; n++) {
-                        if (matrix3x3[m][n] == nRan) {
-                            matrix3x3[i][j] = azar ();
-                        }
-                    }
-                }
-
-
-            }// for-j
-        }// for-i
+        int [][] matrix3x3 = generarMatrizAleatoria ();
 
         for ( int i = 0 ; i < matrix3x3.length ; i++ ) {
             System.out.println ( Arrays.toString ( matrix3x3[i] ) );
@@ -55,8 +13,44 @@ public class Ejercicio4 {
 
     }// fin main
 
+    // Función para generar una matriz de 3x3 con números aleatorios sin repetir
+    public static int[][] generarMatrizAleatoria() {
+        int[][] matriz = new int[3][3];
+
+        // Array para llevar un registro de los números generados
+        boolean[] numerosGenerados = new boolean[10];
+
+        // Rellenar la matriz
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                int numeroAleatorio;
+                do {
+                    // Generar un número aleatorio entre 0 y 9
+                    numeroAleatorio = azar ();
+                } while (numerosGenerados[numeroAleatorio]);
+
+                // Almacenar el número en la matriz y marcar como generado
+                matriz[i][j] = numeroAleatorio;
+                numerosGenerados[numeroAleatorio] = true;
+            }
+        }
+
+        return matriz;
+    }
+
     private static int azar (){
         return (int) (Math.random () * 9);
     }// fin azar
+
+    private static boolean contieneNumero (int [][] array2D , int num){
+        for ( int i = 0 ; i < array2D.length ; i++ ) {
+            for ( int j = 0 ; j < array2D[i].length ; j++ ) {
+                if ( array2D[i][j] == num ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }// fin contieneNumero
 
 }// fin-class Ejercicio4
