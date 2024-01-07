b9a09b59382407be68be091eec1abb80d6c9969c
Andoru <bravocastro.andresmartin@gmail.com>
2024-01-07 21:19:32 +0100

    Versión 7.1.24 EjerciciosUT04 Strings (1-10)

---
+++ BancoDeEjercicios/src/ActividadesUT04_Strings/EjercicioTexto10.java
@@ -0,0 +1,29 @@
+package ActividadesUT04_Strings;
+
+public class EjercicioTexto10 {
+
+    public static void main ( String[] args ) {
+
+        int n = 1001;
+        System.out.println (capicua ( n ));
+
+    }// fin main
+
+    /**
+     * Función que devuelve true si el número es capicua y false si no es capicua
+     * @param numero numero que se va a analizar
+     * @return true si es capicua
+     */
+    private static boolean capicua(int numero){
+        String num = Integer.toString ( numero );
+        StringBuilder ninv = new StringBuilder ();
+        int cont = 0;
+        for ( int i = num.length () ; i > 0 ; i-- ) {
+            ninv.append ( num.charAt ( cont ) );
+            cont++;
+        }
+
+        return ninv.toString().equals( num );
+    }// fin capicua
+
+}// fin-class EjercicioTexto10
