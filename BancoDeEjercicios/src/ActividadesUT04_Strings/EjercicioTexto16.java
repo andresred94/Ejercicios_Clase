fc6c68fe92096809c4ce63a648751fb044dbc473
Andoru <bravocastro.andresmartin@gmail.com>
2024-01-10 17:13:20 +0100

    Versión 10.1.24 EjerciciosUT04 Strings (1-15)

--- BancoDeEjercicios/src/ActividadesUT04_Strings/EjercicioTexto16.java
+++ BancoDeEjercicios/src/ActividadesUT04_Strings/EjercicioTexto16.java
@@ -8,17 +8,24 @@ public class EjercicioTexto16 {
 
     }// fin main
 
+    /**
+     * Función que devuelve un boolean si el número de DNI es correcto
+     * @param nif número de DNI que se va a nalizar
+     * @return true si el número de DNI es correcto
+     */
     private static boolean esValidoNIF(String nif){
+        // guardamos la letra
         char letra = nif.charAt ( nif.length () - 1 );
         int indcLetra = 0;
+        // String que contiene todas las letras en orden
         String letrasDNI = "TRWAGMYFPDXBNJZSQVHLCKE";
-
+        // Recorremos el String y guardamos la posicion en donde se encuentra
         for( int i = 0 ; i < letrasDNI.length (); i++ ) {
             if ( letrasDNI.charAt ( i ) == letra ){
                 indcLetra = i;
             }
         }
-
+        // Convertimos el String a int para poder realizar la division
         int parteEntera = 0;
         for ( int i = 0 ; i < nif.length () - 1 ; i++ ) {
             parteEntera = parteEntera * 10 + Character.getNumericValue ( nif.charAt ( i ) ) ;
@@ -26,7 +33,4 @@ public class EjercicioTexto16 {
 
         return parteEntera % 23 == indcLetra;
     }// fin esValidoNIF
-
-
-
 }// fin-class EjercicioTexto16
