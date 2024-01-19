d50c0457405f72e407014cd51005ec831cb61bae
Andoru <bravocastro.andresmartin@gmail.com>
2024-01-19 17:43:15 +0100

    Version 19.01.24 Introducción a programación orientada a objetos - Creando Tests

--- BancoDeEjercicios/src/LaboratorioDePruebas/Arrays/Array1DSuma.java
+++ BancoDeEjercicios/src/LaboratorioDePruebas/Arrays/Array1DSuma.java
@@ -1,35 +1,27 @@
 package LaboratorioDePruebas.Arrays;
 
-import java.util.Arrays;
-
 public class Array1DSuma {
+    // atributos
+    private int [] rsp;
 
-    public static void main(String[] args) {
-        int [] Array1 =  {1,2,3,4,5};
-        int [] Array2 =  {1,2,3,4,5};
-        sumaArrays(Array1,Array2);
+    // contructores
+    public Array1DSuma(){}
+
+    // settes y getters
+    public void setRsp ( int[] rsp ) {
+        this.rsp = rsp;
+    }
+    public int[] getRsp () {
+        return rsp;
     }
 
-    public static void sumaArrays (int [] array1 , int [] array2){
-
-        int [] sumaArrays = new int [array1.length];
-        for (int i = 0; i < sumaArrays.length; i++) {
-            sumaArrays[i] = array1[i] + array2[i];
+    // métodos de la clase
+    public int [] sumaArrays (int [] array1 , int [] array2){
+        rsp = new int [array1.length];
+        for (int i = 0; i < array1.length; i++) {
+            rsp[i] = array1[i] + array2[i];
         }
 
-        System.out.println(Arrays.toString(sumaArrays));
-        for (int i = 0; i < sumaArrays.length; i++) {
-            System.out.printf("%d + %d = %d%n", array1[i] , array2[i] , sumaArrays[i] );
-        }
-
-        /*
-        for (int i = 0; i < sumaArrays.length; i++) {
-            System.out.printf("Valor posicion array1[%d] = %d%n", i + 1, array1[i]);
-            System.out.printf("Valor posicion array2[%d] = %d%n", i + 1, array2[i]);
-            System.out.printf("Valor posicion sumaArrays[%d] %d%n", i + 1 , sumaArrays[i]);
-        }*/
-
-
+        return rsp;
     }// fin SumaArrays
-
 }// Array1DSuma
---
+++ BancoDeEjercicios/tests/LaboratorioDePruebas/Arrays/Array1DSumaTest.java
@@ -0,0 +1,18 @@
+package LaboratorioDePruebas.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Array1DSumaTest {
+    Array1DSuma a1 = new Array1DSuma ();
+    int [] Array1 =  {1,2,3,4,5};
+    int [] Array2 =  {1,2,3,4,5};
+
+    @Test
+    void sumaArrays () {
+        int [] resultado = a1.sumaArrays ( Array1 , Array2 );
+        int [] esperado = {2,4,6,8,10};
+        assertArrayEquals ( esperado , resultado );
+    }
+}
